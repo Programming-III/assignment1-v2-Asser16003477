24f1bf77168d4a2bdcd6c4957e4fd1d9d7c34515
Asser16003477 <asserehabkhattab@gmail.com>
2025-11-11 09:51:57 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -6,6 +6,22 @@ using namespace std;
 
 //#write Person class here
 
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+using namespace std;
+class Person{
+    private:
+    string name;
+    int id;
+    public:
+        Person(string name, int id);
+        void display();
+};
+
+
+#endif // PERSON_H_INCLUDED
 
 
 
