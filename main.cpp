47d0fc4c92aab361c8347d4da4373b69bac74faf
Asser16003477 <asserehabkhattab@gmail.com>
2025-11-11 09:53:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,26 @@ using namespace std;
 // ==================== Person Class Implementation =========================
 
 
+Person:: Person(string name, int id){
+this->name=name;
+this->id=id;
+}
+Person:: Person(){
+    name="Omar Nabil";
+    id="2202";
+}
+Person:: void display(){
+cout<<"name: "<<name<<endl;
+cout<<"id: "<<id<<endl;
+
+}
+ Person::string getName(){
+ return name;
+ }         
+Person:: string getId(){
+return id;
+}
+
 
 
 
@@ -19,7 +39,24 @@ using namespace std;
 
 // ==================== Student Class Implementation ====================
 
-
+Person:: Class(int yearLevel, string major){
+this->yearLevel=yearLevel;
+this->major=major;
+}
+Person:: Class(){
+    yearLevel="2";
+    major="Informatics";
+}
+Class:: void display(){
+cout<<"yearLevel: "<<yearLevel<<endl;
+cout<<"major: "<<major<<endl;
+}
+Class::int getYearLevel(){
+    return yearLevel;
+}
+Class::string major(){
+    return major;
+}
 
 
 
@@ -27,7 +64,11 @@ using namespace std;
 // ==================== Instructor Class Implementation ====================
 
 
-
+Person:: Instructor(string department, int exprienceYears){
+this->department=department;
+this->exprienceYears=exprienceYears;
+}
+Person:: Instructor
 
 
 
