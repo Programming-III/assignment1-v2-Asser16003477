4dda96143055f26dc1d7430408bed4f0934a50b0
Asser16003477 <asserehabkhattab@gmail.com>
2025-11-11 09:49:30 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -6,7 +6,26 @@
 using namespace std;
 //#write your code here
 
+#ifndef COURSE_H
+#define COURSE_H
 
+#include <string>
+#include "Student.h"
+using namespace std;
+class Course{
+private:
+string courseCode;
+string courseName;
+int maxStudents;
+Student* students;
+int currentStudents;
+public:
+    Course(string courseCode, string courseName,int maxStudents, Student* students,int currentStudents );
+    addstudent(const Student& s);
+    void displayCourseInfo();
+    
+    
+};
 
 
 
