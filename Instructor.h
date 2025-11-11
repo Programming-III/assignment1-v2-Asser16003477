ed102be9eda4f6c5c53682e6dc3f1ea1b7fd3868
Asser16003477 <asserehabkhattab@gmail.com>
2025-11-11 09:50:09 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -7,6 +7,32 @@ using namespace std;
 
 
 
+#ifndef INSTRUCTOR_H
+#define INSTRUCTOR_H
+
+#include "Person.h"
+using namespace std;
+class Instructor{
+    string department;
+    int exprienceYears;
+public:
+    Instructor(string department,int exprienceYears);
+    void display;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
 
 
 
